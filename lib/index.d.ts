37548e043c33f952abe786fd6106074ab28b1f55
Leo <monkeydreamgo@gmail.com>
2021-01-17 19:08:54 +0800

    update project config

--- lib/index.d.ts
+++
@@ -1,8 +0,0 @@
-export { default as AutoComplete } from './src/components/AutoComplete';
-export { default as Button } from './src/components/Button';
-export { default as Icon } from './src/components/Icon';
-export { default as Input } from './src/components/Input';
-export { default as Menu } from './src/components/Menu';
-export { default as Progress } from './src/components/Progress';
-export { default as Transition } from './src/components/Transition';
-export { default as Upload } from './src/components/Upload';
