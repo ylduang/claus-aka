418598bdc6f12e118929a98bd861a88da184f9a0
Leo <monkeydreamgo@gmail.com>
2021-01-25 20:26:59 +0800

    run storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -24,12 +24,12 @@
 
 module.exports = {
   stories: [
-    "../src/**/**/*.stories.mdx",
-    "../src/**/**/*.stories.@(js|jsx|ts|tsx)",
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)",
     // "../src/styles/index.scss",
     "../stories/**/*.stories.mdx",
-    "../stories/**/**/*.stories.mdx",
-    "../stories/**/**/*.stories.@(js|jsx|ts|tsx)",
+    "../stories/**/*.stories.mdx",
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)",
     // "../stories/**/**/*.scss",
   ], // 配置编译文件的入口
   addons: [
@@ -37,37 +37,37 @@ module.exports = {
     "@storybook/addon-essentials",
     "@storybook/addon-actions",
   ], // 配置使用的官方提供的插件
-  webpackFinal: async (config) => {
-    // do mutation to the config
-    // 支持ts，tsx语法
-    config.module.rules.push({
-      test: /\.(ts|tsx)$/,
-      use: [
-        {
-          loader: require.resolve("ts-loader"),
-        },
-        // Optional
-        {
-          loader: require.resolve("react-docgen-typescript-loader"),
-          options: {
-            shouldExtractLiteralValuesFromEnum: true, // 枚举联合类型不显示
-            propFilter: (prop, component) => {
-              if (prop.parent) {
-                return !prop.parent.fileName.includes("node_modules");
-              }
+  // webpackFinal: async (config) => {
+  //   // do mutation to the config
+  //   // 支持ts，tsx语法
+  //   config.module.rules.push({
+  //     test: /\.(ts|tsx)$/,
+  //     use: [
+  //       {
+  //         loader: require.resolve("ts-loader"),
+  //       },
+  //       // Optional
+  //       {
+  //         loader: require.resolve("react-docgen-typescript-loader"),
+  //         options: {
+  //           shouldExtractLiteralValuesFromEnum: true, // 枚举联合类型不显示
+  //           propFilter: (prop, component) => {
+  //             if (prop.parent) {
+  //               return !prop.parent.fileName.includes("node_modules");
+  //             }
 
-              return true;
-            },
-          },
-        },
-      ],
-    });
-    // 支持scss文件，其他样式文件，在加入
-    config.module.rules.push({
-      test: /\.scss$/,
-      use: ["style-loader", "css-loader", "sass-loader"],
-    });
-    config.resolve.extensions.push(".ts", ".tsx");
-    return config;
-  },
+  //             return true;
+  //           },
+  //         },
+  //       },
+  //     ],
+  //   });
+  //   // 支持scss文件，其他样式文件，在加入
+  //   config.module.rules.push({
+  //     test: /\.scss$/,
+  //     use: ["style-loader", "css-loader", "sass-loader"],
+  //   });
+  //   config.resolve.extensions.push(".ts", ".tsx");
+  //   return config;
+  // },
 };
